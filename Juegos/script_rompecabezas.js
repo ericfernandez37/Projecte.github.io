e71a019860d7d847c1065e21464c5e4d4b5d4b31
Cristian <cristian.crf.crf@gmail.com>
2020-12-11 14:44:24 +0100

    modificaciones rompecabezas

--- Juegos/script_rompecabezas.js
+++ Juegos/script_rompecabezas.js
@@ -1,3 +1,4 @@
+/*Cronometro*/ 
 var inicio=0;
 	var timeout=0;
  
@@ -39,26 +40,25 @@ var inicio=0;
 		timeout=setTimeout("funcionando()",1000);
 	}
  
-	/* Funcion que pone un 0 delante de un valor si es necesario */
+    /* Funcion que pone un 0 delante de un valor si es necesario */
 	function LeadingZero(Time) {
 		return (Time < 10) ? "0" + Time : + Time;
   }
-  
-  /*puzzle*/
 
-  var piezas = document.getElementsByClassName("movil");
 
-  var tamwidth = [134,192,134,163,134,163,134,192,134];
-  var tamheight = [164,134,163,134,192,134,163,134,163];
+/*Puzzle*/ 
+var piezas = document.getElementsByClassName('movil');
 
-  for(var i=0; i<piezas.length;i++){
-    piezas[i].setAttribute("width", tamwidth[i]);
-    piezas[i].setAttribute("height", tamheight[i]);
-    piezas[i].setAttribute("x",Math.floor((Math.random() + 10) + 1));
-    piezas[i].setAttribute("y",Math.floor((Math.random() + 209) + 1));
-    piezas[i].setAttribute("onmousedown","seleccionarElemento(evt)");
-  }
+var tamwidth = [230,1800,230,230,230,230,230,150,230];
+var tamheight = [200,180,200,200,200,200,230,250,200];
 
+for(var i=0;i<piezas.length;i++){
+    piezas[i].setAttribute("width",tamwidth[i]);
+    piezas[i].setAttribute("height",tamheight[i]);
+    piezas[i].setAttribute("x", Math.floor((Math.random() * 10) * 1));
+    piezas[i].setAttribute("y", Math.floor((Math.random() * 409) * 1));
+    piezas[i].setAttribute("onmousedown","seleccionarelemento(evt)");
+}
 
 var elementSelect = 0;
 var currentX = 0;
@@ -66,70 +66,73 @@ var currentY = 0;
 var currentPosX = 0;
 var currentPosY = 0;
 
-
-function seleccionarElemento(evt){
-  elementSelect = reordenar(evt);
-  currentX = evt.clientX;
-  currentY = evt.clientY;
-  currentPosX = parseFloat(elementSelect.getAttribute("x"));
-  currentPosY = parseFloat(elementSelect.getAttribute("y"));
-  elementSelect.setAttribute("onmousemove","moverElemento(evt)");
+function seleccionarelemento(evt) {
+    elementSelect = reordenar(evt);
+    currentX = evt.clientX;
+    currentY = evt.clientY;
+    currentPosx = parseFloat(elementSelect.getAttribute("x"));
+    currentPosy = parseFloat(elementSelect.getAttribute("y"));
+    elementSelect.setAttribute("onmousemove","moverelemento(evt)");
 }
 
-function moverElemento(evt){
-  var dx = evt.clientX - currentX;
-  var dy = evt.clientY - currentY;
-  currentPosX  = currentPosX + dx;
-  currentPosY  = currentPosY + dy;
-  elementSelect.setAttribute("x",currentPosX);
-  elementSelect.setAttribute("y",currentPosY);
-  currentX = evt.clientX;
-  currentY = evt.clientY;
-  elementSelect.setAttribute("onmouseout","deseleccionarElemento(evt)");
-  elementSelect.setAttribute("onmouseup","deseleccionarElemento(evt)");
-  
+function moverelemento(evt){
+    var dx = evt.clientX - currentX;
+    var dy = evt.clientY - currentY;
+    currentPosx = currentPosx + dx;
+    currentPosy = currentPosy + dy;
+    elementSelect.setAttribute("x",currentPosx);
+    elementSelect.setAttribute("y",currentPosy);
+    currentX = evt.clientX;
+    currentY = evt.clientY;
+    elementSelect.setAttribute("onmouseout","deseleccionarelemento(evt)");
+    elementSelect.setAttribute("onmouseup","deseleccionarelemento(evt)");
+    iman();
 }
 
-function deseleccionarElemento(evt){
-  if(elementSelect != 0){
-    elementSelect.removeAttribute("onmousemove");
-    elementSelect.removeAttribute("onmouseout");
-    elementSelect.removeAttribute("onmouseup");
-    elementSelect = 0;
-
-
-  }
+function deseleccionarelemento(evt){
+    if(elementSelect != 0){
+        elementSelect.removeAttribute("onmousemove");
+        elementSelect.removeAttribute("onmousemout");
+        elementSelect.removeAttribute("onmouseup");
+        elementSelect = 0;
+    }
 }
 
-var imagenfondofondo = document.getElementById('imagenfondo');
+var entorno = document.getElementById('entorno');
 
 function reordenar(evt){
-  var padre = evt.target.parentNode;
-  var clone = padre.cloneNode(true);
-  var id = padre.getAttribute("id");
-  imagenfondo.removeChild(document.getElementById(id));
-  imagenfondo.appendChild(clone);
-  return imagenfondo.lastChild.firstChild;
-
+    var padre = evt.target.parentNode;
+    var clone = padre.cloneNode(true);
+    var id = padre.getAttribute("id");
+    entorno.removeChild(document.getElementById(id));
+    entorno.appendChild(clone);
+    return entorno.lastChild.firstChild;
 }
 
-var origX = [200,304,466,200,333,437,200,304,466];
-var origY = [100,100,100,233,204,233,337,266,337];
+var origX = [200,304,466,200,333,437,200,304,466];   
+var origY = [100,100,100,233,204,233,337,366,337];
 
-var vict = document.getElementById("victoria");
-
-function testing(){
-  var bien_ubicada = 0;
-  var padres = document.getElementsByClassName('pade');
-  for(var i=0; i<piezas.length;i++){
-    var posx = parseFloat(padres[i].firstChild.getAttribute("x"));
-    var posy = parseFloat(padres[i].firstChild.getAttribute("y"));
-    ide = padres[i].getAttribute("id");
-    if(origX[ide] == posx && origY[ide] == posy){
-        bien:ubicada = bien_ubicada + 1;
-    }
-  }
+function iman(){
+	for(var i=0;i<piezas.length;i++){
+		if (Math.abs(currentPosx-origX[i])<15 && Math.abs(currentPosy-origY[i])<15) {
+			elementSelect.setAttribute("x",origX[i]);
+			elementSelect.setAttribute("y",origY[i]);
+		}
+	}
 }
 
-
-
+function testing() {
+	var bien_ubicada = 0;
+	var padres = document.getElementsByClassName('padre');
+	for(var i=0;i<piezas.length;i++){
+		var posx = parseFloat(padres[i].firstChild.getAttribute("x"));    
+		var posy = parseFloat(padres[i].firstChild.getAttribute("y"));
+		ide = padres[i].getAttribute("id");
+		if(origX[ide] == posx && origY[ide] == posy){
+			bien_ubicada = bien_ubicada + 1;
+		}
+	}
+	if(bien_ubicada == 9){
+		win.play();
+	}
+}
